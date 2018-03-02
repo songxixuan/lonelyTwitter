6436ce9ce427cf4fc7a7b4d71d3cb08cd31441fa
songxixuan <33207680+songxixuan@users.noreply.github.com>
2018-03-02 15:40:48 -0700

    lab7

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -1,7 +1,9 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EditTweetActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class EditTweetActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_tweet);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("tweetMessage");
+
+        TextView textView = (TextView)findViewById(R.id.messageTV);
+        textView.setText(message);
     }
 }
